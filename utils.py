545cf847def2f9a47f3163ee2b659356a6ee388a
Carlos Pumar <carlospumar@carloss-mbp.localdomain>
2023-01-04 17:16:21 +0100

    add initial attributes and methods for BikeRentModeler()

--- utils.py
+++ utils.py
@@ -1,27 +1,42 @@
-import pandas as pd
 import pytest
+
+import pandas as pd
+import numpy as np
+
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_log_error
 
-#TODO make function for reading in both datasets
-transformable = pd.read_csv('./data/train.csv', index_col = 0, parse_dates = True)
-
-
-PATH = './artifacts/X_fe_col_names.csv'
+PATH_INITIAL_DATA = '../data/'
+PATH_TRANSFORMED_DATA = '../artifacts/'
 
 #TODO think about how to apply everything to both train as well as test
 class BikeRentModeler():
     '''Read, split, transform, fit and predict.'''
 
-    def __init__(self, path = PATH) -> None:
-        self.path = path
+    #TODO: check syntax attributes (i.e. drop hard-coded attributes)
+    def __init__(self) -> None:
+        self.path_initial = PATH_INITIAL_DATA
+        self.path_transformed = PATH_TRANSFORMED_DATA
 
+    #TODO: check syntax placeholders; check return statement! 
     def prepare_data(self) -> dict:
         '''Returns dict with feature matrix and labels as values.'''
-        df = pd.read_csv(self.path, index_col=0, parse_dates=True)
-        X = df.drop(['remainder__Client_Status_Post3Months', 'remainder__Client_Status_Post6Months'], axis=1) 
-        y = df['remainder__Client_Status_Post3Months']
-        return {'feature_matrix': X, 'labels': y}
+        loadable = ['Xtrain', 'Xval']
+        for data in loadable:
+            loaded = list()
+            df_train, df_val = pd.read_csv("self.path_initial/'%s'.csv", data, index_col=0, parse_dates=True)
+            loaded.append(df_train)
+            loaded.append(df_val)
+            for data_loaded in loaded:
+                X_train, X_val = data_loaded.drop(['casual', 'registered', 'count'], axis=1) 
+                y_train, y_val = data_loaded['count']
+        return {
+            'X_train': X_train, 
+            'X_val': X_val,
+            'y_train': y_train,
+            'y_val': y_val,
+            }
 
     def include_timestamps(self, df) -> pd.DataFrame:
         '''Returns DataFrame with time-stamps.'''
@@ -29,17 +44,19 @@ class BikeRentModeler():
         df['Month'] = df.index.month
         return df
 
-    def split_timestamp_data(self, X, y) -> dict:
-        '''Returns dict consisting of split data (incl. timestamps).'''
-        X_train, X_val, y_train, y_val = train_test_split(X, y, random_state = 42)
-        X_train_timestamped = self.include_timestamps(X_train)
-        X_val_timestamped = self.include_timestamps(X_val)
-        return {
-            'X_train_fe': X_train_timestamped, 
-            'X_val_fe': X_val_timestamped,
-            'y_train': y_train,
-            'y_val': y_val,
-            }
+    #TODO: check if split is even necessary (data has already been split!).
+    # Also: implement manual split based on dates (<=19th and 19th < dates <=30)?
+    # def split_timestamp_data(self, X, y) -> dict:
+    #     '''Returns dict consisting of split data (incl. timestamps).'''
+    #     X_train, X_val, y_train, y_val = train_test_split(X, y, random_state = 42)
+    #     X_train_timestamped = self.include_timestamps(X_train)
+    #     X_val_timestamped = self.include_timestamps(X_val)
+    #     return {
+    #         'X_train_fe': X_train_timestamped, 
+    #         'X_val_fe': X_val_timestamped,
+    #         'y_train': y_train,
+    #         'y_val': y_val,
+    #         }
 
     def model_fit(self, X_train, y_train):
         '''Returns fitted Linear ression model.'''
@@ -56,17 +73,26 @@ class BikeRentModeler():
     def interpolator():
         '''Fill hourly vals for registered and casual.'''
         pass
-
-    def label_transformer():
-        '''Transforms labels to logged vals.'''
-        pass
-
-    def bring_back_transformer():
-        '''Transforms predictions to unlogged vals.'''
-        pass
-
-    def metric_builder():
+    
+    def metric_builder(predictions) -> np.array:
         '''Calculates root mean square logged error.'''
-        pass
+        return np.sqrt(mean_squared_log_error(predictions))
+
+    #TODO check np method as well as placeholder
+    def label_transformer(y_train, y_val) -> pd.Series:
+        '''Transforms labels to logged vals.'''
+        labels = [y_train, y_val]
+        for label in labels:
+            y_train_logged, y_val_logged = np.log1p('%s', label) - 1
+        return y_train_logged, y_val_logged
+
+    #TODO check np method as well as placeholder
+    def bring_back_transformer(y_train_logged, y_val_logged):
+        '''Transforms predictions to unlogged vals.'''
+        undone_logged_labels = [y_train_logged, y_val_logged]
+        for logged_label in undone_logged_labels:
+            y_train_unlogged, y_val_unlogged = np.exp('%s', logged_label)
+        return y_train_unlogged, y_val_unlogged
+
 
 
